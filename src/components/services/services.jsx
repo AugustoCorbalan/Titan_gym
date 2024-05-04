677c72a1853caebc4a775c4c22d2c27357b378d4
unknown <auguscorbi@gmail.com>
2024-05-04 12:57:18 -0300

    Diseño 'back' de tajetas sección 'services'

--- src/components/services/services.jsx
+++ src/components/services/services.jsx
@@ -3,14 +3,51 @@ export const Services = ()=>{
         <div className="services_container">
             <h2>¿Que ofrecemos? </h2>
             <div className="container_cards">
-                <div>
-                    <span>ENTRENAMIENTO <br/> PERSONALIZADO</span>
+                <div className="container_card_horizontal">
+                    <div className="container_card_inner">
+                        <div id="personalizado_front" className="front-end">
+                            <span>ENTRENAMIENTO <br/> PERSONALIZADO</span>
+                        </div>
+                        <div className="back-end">
+                            <span>BACK  </span>
+                        </div>
+                    </div>
                 </div>
-                <div>
-                    <span>TOTAL BODY</span>
+                <div className="container_card_vertical">
+                    <div className="container_card_inner">
+                        <div id="total_body_front" className="front-end">
+                            <span>TOTAL BODY</span>
+                        </div>
+                        <div className="back-end">
+                            <div className="container_info">
+                                <div className="container_title">
+                                    <h3> TOTAL BODY</h3>
+                                </div>
+                                <div className="container_description">
+                                    <ul>
+                                        <li>Fortalecimiento de músculos estabilizadores</li>
+                                        <li>Mejora del equilibrio</li>
+                                        <li>Coordinación y flexibilidad</li>
+                                        <li>Prevención de lesiones. </li>
+                                        <li>Mejora de rendimiento en actividades deportivas</li>
+                                    </ul>
+                                </div>
+                                <div className="container_button">
+                                    <button>MAS INFO</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div>
-                    <span>BOXEO</span>
+                <div className="container_card_horizontal">
+                    <div className="container_card_inner">
+                        <div id="boxeo_front" className="front-end">
+                            <span>BOXEO</span>
+                        </div>
+                        <div className="back-end">
+                            <span>BACK  </span>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
