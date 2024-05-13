d3ba536e3f0defd227c56904592fd6d18610ada5
unknown <auguscorbi@gmail.com>
2024-05-12 22:40:07 -0300

    Estilos para mobile de la sección about

--- src/App.js
+++ src/App.js
@@ -13,9 +13,9 @@ function App() {
       </header>
       <Landing/>
       <About/>
-      <Services/>
+      {/* <Services/>
       <TimeTable/>
-      <Footer/>
+      <Footer/> */}
     </div>
   );
 }
