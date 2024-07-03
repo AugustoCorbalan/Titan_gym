3df777d8e522a632d3f66cbf826d4c20520ebb9f
unknown <auguscorbi@gmail.com>
2024-07-03 13:01:08 -0300

    correccion en la funcionalidad de los botones del navbar

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -6,16 +6,23 @@ export const Navbar = ()=>{
     const setScrollTop0 = ()=>{
         window.scrollTo(0,0);
     }
+    const scroll = (section)=>{
+        const sectionElement = document.getElementById(section);
+        const sectionTop = sectionElement.getBoundingClientRect().top;
+        const scrollWindow = window.scrollY;
+        console.log(sectionTop);
+        window.scrollTo(0, sectionTop + scrollWindow);
+    }
     return(
         <div className='navbar_container'>
-            <a className='container_logo' onClick={setScrollTop0}>
+            <button className='button_scroll container_logo' onClick={setScrollTop0}>
                 <img src={primaryLogo} alt="Logotipo de marca"/>
-            </a>
-            <div className='container_a'>
-                <a href='#about_section'>¿Quienes Somos?</a>
-                <a href='#trainings_section'>Entrenamientos</a>
-                <a href='#hours_section'>Horarios</a>
-                <a>Tienda</a>
+            </button>
+            <div className='container_buttons'>
+                <button className='button_scroll' onClick={()=>scroll('about_section')}>¿Quienes Somos?</button>
+                <button className='button_scroll' onClick={()=>scroll('trainings_section')}>Entrenamientos</button>
+                <button className='button_scroll' onClick={()=>scroll('hours_section')}>Horarios</button>
+                <button className='button_scroll'>Tienda</button>
             </div>
             <div className='container_a_mobile'>
                 <button>
