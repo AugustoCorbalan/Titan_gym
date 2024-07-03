5d828bf76372ae157fb67ab284f097b3d71bf284
unknown <auguscorbi@gmail.com>
2024-07-03 16:51:35 -0300

    carteles de proximamente en botones de funcionalidades futuras

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,8 +1,22 @@
 import primaryLogo from '../../assets/logos/primaryLogo.svg';
 import user_circle from '../../assets/icons/user_circle.svg';
 import dots_vertical from '../../assets/icons/dots_vertical.svg'; 
+import { useState } from 'react';
 
 export const Navbar = ()=>{
+
+    const [visibilityPosterStore, setVisibilityPosterStore] = useState(false);
+    const [visibilityPosterLogin, setVisibilityPosterLogin] = useState(false);
+
+    const handlerVisibilityPosterStore = ()=>{
+        setVisibilityPosterStore(!visibilityPosterStore);
+        setTimeout(()=>{setVisibilityPosterStore(false)}, 3000)
+    }
+    const handlerVisibilityPosterLogin = ()=>{
+        setVisibilityPosterLogin(!visibilityPosterLogin);
+        setTimeout(()=>{setVisibilityPosterLogin(false)}, 3500)
+    }
+
     const setScrollTop0 = ()=>{
         window.scrollTo(0,0);
     }
@@ -22,7 +36,12 @@ export const Navbar = ()=>{
                 <button className='button_scroll' onClick={()=>scroll('about_section')}>¿Quienes Somos?</button>
                 <button className='button_scroll' onClick={()=>scroll('trainings_section')}>Entrenamientos</button>
                 <button className='button_scroll' onClick={()=>scroll('hours_section')}>Horarios</button>
-                <button className='button_scroll'>Tienda</button>
+                <button className='button_scroll' onClick={handlerVisibilityPosterStore}>
+                    Tienda
+                </button>
+                <div className={visibilityPosterStore? 'upcoming_poster' : 'upcoming_poster_none'}>
+                    <span>¡Proximamente podrás adquirir todos los productos de <p>TITAN </p> directamente desde acá!</span>
+                </div>
             </div>
             <div className='container_a_mobile'>
                 <button>
@@ -30,7 +49,12 @@ export const Navbar = ()=>{
                 </button>
             </div>
             <div className='container_icons'>
-                <img src={user_circle} alt="Icono del perfil del usuario"/>
+                <button onClick={handlerVisibilityPosterLogin}>
+                    <img src={user_circle} alt="Icono del perfil del usuario"/>
+                </button>
+            </div>
+            <div className={visibilityPosterLogin? 'upcoming_poster upcoming_poster_login' : 'upcoming_poster_none'}>
+                <span>¡Proximamente podrás iniciar sesión y tener un seguimiento de tus compras y progresos!</span>
             </div>
         </div>
     )
