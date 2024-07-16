05c16dffe11aac4d11e6b332a53a4f2b1d462ea1
unknown <auguscorbi@gmail.com>
2024-07-16 18:24:47 -0300

    Agrego footer a la sección shop

--- src/components/shop/Shop.jsx
+++ src/components/shop/Shop.jsx
@@ -1,5 +1,7 @@
-import { Navbar } from "../navbar/navbar.jsx"
-import { ContainerCards } from "./components/containerCards.jsx"
+import { Navbar } from "../navbar/navbar.jsx";
+import {Footer} from "../footer/footer.jsx";
+import { ContainerCards } from "./components/containerCards.jsx";
+
 export const Shop = ()=>{
     return(
         <>
@@ -7,6 +9,7 @@ export const Shop = ()=>{
             <div className="shop_container">
                 <ContainerCards/>
             </div>
+            <Footer/>
         </>
     )
 }
